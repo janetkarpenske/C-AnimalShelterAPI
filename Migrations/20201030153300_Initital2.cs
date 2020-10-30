1f0636f8a60cb01c1768945e23ad1314afaddee7
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-30 08:33:54 -0700

    Set up files and run initial migration

---
+++ Migrations/20201030153300_Initital2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Solution.Migrations
+{
+    public partial class Initital2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
