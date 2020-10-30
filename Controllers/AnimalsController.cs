532362d57dfd343a38f065d0a546cf6dac243bb0
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-30 09:18:27 -0700

    Add route to get animals by id

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+    private AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/animals
+    [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get()
+    {
+      return _db.Animals.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
+    public void Post([FromBody] Animal animal)
+    {
+      _db.Animals.Add(animal);
+      _db.SaveChanges();
+    }
+
+    // GET api/animals/5
+  [HttpGet("{id}")]
+  public ActionResult<Animal> Get(int id)
+  {
+      return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+  }
+  }
+}
