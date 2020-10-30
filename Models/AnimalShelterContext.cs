4f6ad867ff343991be04f52009352b45af20c166
Janet Karpenske <janetkarpenske@gmail.com>
2020-10-30 11:13:46 -0700

    Add seed data and fix formatting and errors

--- Models/AnimalShelterContext.cs
+++ Models/AnimalShelterContext.cs
@@ -12,11 +12,13 @@ namespace AnimalShelter.Models
         {
         builder.Entity<Animal>()
             .HasData(
-                new Animal { AnimalId = 1, Name = "Matilda", Species = "Woolly Mammoth", Age = 7, Gender = "Female" },
-                new Animal { AnimalId = 2, Name = "Rexie", Species = "Dinosaur", Age = 10, Gender = "Female" },
-                new Animal { AnimalId = 3, Name = "Matilda", Species = "Dinosaur", Age = 2, Gender = "Female" },
-                new Animal { AnimalId = 4, Name = "Pip", Species = "Shark", Age = 4, Gender = "Male" },
-                new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Dinosaur", Age = 22, Gender = "Male" }
+                new Animal { AnimalId = 1, Name = "Benji", Species = "Dog", Breed = "Golden Retriever", Age = 5, Gender = "Male" },
+                new Animal { AnimalId = 2, Name = "Holly", Species = "Dog", Breed = "Chocolate Lab", Age = 2, Gender = "Female" },
+                new Animal { AnimalId = 3, Name = "Penny", Species = "Cat", Breed = "Domestic Long-Hair", Age = 2, Gender = "Female" },
+                new Animal { AnimalId = 4, Name = "Cookie", Species = "Cat", Breed = "Domestic Short-Hair", Age = 4, Gender = "Male" },
+                new Animal { AnimalId = 5, Name = "Krull the Terrible", Species = "Dog", Breed = "Chihuahua", Age = 8, Gender = "Male" },
+                new Animal { AnimalId = 6, Name = "Munchkin", Species = "Cat", Breed = "Ragdoll", Age = 1, Gender = "Male" },
+                new Animal { AnimalId = 7, Name = "Izzie", Species = "Dog", Breed = "Terrier", Age = 2, Gender = "Female" }
             );
         }
         public DbSet<Animal> Animals { get; set; }
